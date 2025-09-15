1fae0e7728b28dd1a9e9d86d663df50488d061b6
Dharani <dharanisravani859@gmail.com>
2025-09-15 19:46:08 +0530

    Fixed volunteer_dashboard.dart s, added intl, updated textTheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,47 @@
- 
+import 'package:flutter/material.dart';
+import 'screens/volunteer/volunteer_dashboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MedCluster',
+      home: const HomeScreen(), // default landing page
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('MedCluster Home')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to VolunteerDashboard
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => const VolunteerDashboard(
+                  volunteerId: 'vol_001',
+                  volunteerName: 'Siva',
+                ),
+              ),
+            );
+          },
+          child: const Text('Go to Volunteer Dashboard'),
+        ),
+      ),
+    );
+  }
+}
